3e99f3e878b68eabcc94f824d24247057246a924
Sebastian Kolbe <s.kolbe@lemoe.de>
2015-02-10 17:15:14 +0100

    Logo
    SVG eingefügt und zentriert (noch nicht zu 100% responsive)

--- index.php
+++ index.php
@@ -22,7 +22,17 @@
   <body>
 	<!-- /BEGIN -->
 	<div class="container">
-	wwspree
+		<div class="col-xs-12 col-md-12">
+			<div class="landing">
+				<a href="/" class="logo">wwspree</a>
+				<div class="addy">
+					<h1>Webdesign & Konzeption</h1>
+					<p>Sebastian Kolbe</p>
+					<p>+49 (0) 30 /  123 456 78</p>
+					<a href="&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&#x3a;%69%6E%66%6F%40%77%77%73%70%72%65%65%2E%64%65 "title="&#x45;&#x2d;&#x4d;&#x61;&#x69;&#x6c;">&#x69;&#x6e;&#x66;&#x6f;&#x40;&#x77;&#x77;&#x73;&#x70;&#x72;&#x65;&#x65;&#x2e;&#x64;&#x65;</a>
+				</div>
+			</div>
+		</div>
 	</div>
 	<!-- /END -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
